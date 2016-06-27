222fd3dfa597c9639cd8fdb98474d0623a9a7247
Wei Xia <xiawei@us.ibm.com>
2016-06-27 16:17:32 -0400

    update

---
+++ Lable.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let order = UILabel(frame: CGRectMake(0, 0, 200, 50))
+
+order.text = "Order Now"
+
+order.textColor = UIColor.blackColor()
+
+order.backgroundColor = UIColor.whiteColor()
+
+order.textAlignment = NSTextAlignment.Center
+
+order.layer.cornerRadius = 20
+
+order.clipsToBounds = true
+
+order
